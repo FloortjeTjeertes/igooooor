4fd5b940bc1b0046ded6261db5bf63af41db02cb
florianmac <nairolf3>
2018-12-04 09:53:58 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("zeg 'igor open de deur' om de deur te openen");
+        Scanner wachtwoord = new Scanner(System.in);
+        if (wachtwoord == "igor open de deur")
+        {
+
+        }
+
+
+
+
+        if (igor.opendeur(true));
+         System.out.println("okay master i will open teh door");
+    }
+}
---
+++ src/igor.java
@@ -0,0 +1,7 @@
+public class igor {
+    public static boolean opendeur(boolean b1){
+
+
+        return true;
+    }
+}
